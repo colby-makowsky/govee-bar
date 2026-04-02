af0c84f532338f5f986c750f704ba60383482385
colby-makowsky <colby.makowsky@gmail.com>
2026-04-02 17:25:40 -0400

    Add custom menu bar icons with three states
    Icons from line-md icon pack (lightbulb variants):
    - Filled lightbulb: lights ON (display connected + unlocked)
    - Outline lightbulb: lights OFF (display connected + locked)
    - Strikethrough lightbulb: Studio Display not connected
    
    Static SVGs stripped of animations, rendered as template images
    for proper dark/light mode support.

--- GoveeBar/GoveeBarApp.swift
+++ GoveeBar/GoveeBarApp.swift
@@ -4,11 +4,22 @@ import SwiftUI
 struct GoveeBarApp: App {
     @StateObject private var stateManager = LightStateManager()
 
+    private var menuBarIconName: String {
+        if !stateManager.displayConnected {
+            return "MenuBarIconDisabled"
+        } else if stateManager.lightsOn {
+            return "MenuBarIconOn"
+        } else {
+            return "MenuBarIconOff"
+        }
+    }
+
     var body: some Scene {
         MenuBarExtra {
             MenuBarView(stateManager: stateManager)
         } label: {
-            Image(systemName: stateManager.lightsOn ? "lightbulb.fill" : "lightbulb.slash")
+            Image(menuBarIconName)
+                .renderingMode(.template)
         }
         .menuBarExtraStyle(.menu)
 
